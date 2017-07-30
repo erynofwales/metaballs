209974907115c5fb12b362a81593a6092ed17925
Eryn Wells <eryn@erynwells.me>
2017-07-30 14:04:39 -0700

    Initial Commit

---
+++ Metaballs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Metaballs
+//
+//  Created by Eryn Wells on 7/30/17.
+//  Copyright © 2017 Eryn Wells. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
