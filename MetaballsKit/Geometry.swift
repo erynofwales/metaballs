2441bd0b7634ed043085e788af8b3fd3e3f4779f
Eryn Wells <eryn@erynwells.me>
2017-08-05 11:09:03 -0700

    [MetaballsKit] Add Geometry file

---
+++ MetaballsKit/Geometry.swift
@@ -0,0 +1,41 @@
+//
+//  Geometry.swift
+//  Metaballs
+//
+//  Created by Eryn Wells on 8/5/17.
+//  Copyright © 2017 Eryn Wells. All rights reserved.
+//
+
+import Foundation
+
+public struct Point {
+    var x: Float
+    var y: Float
+
+    var CGPoint: CGPoint {
+        return CoreGraphics.CGPoint(x: CGFloat(x), y: CGFloat(y))
+    }
+
+    init() {
+        self.init(x: 0, y: 0)
+    }
+
+    init(x: Float, y: Float) {
+        self.x = x
+        self.y = y
+    }
+}
+
+public struct Vector {
+    var dx: Float
+    var dy: Float
+
+    init() {
+        self.init(dx: 0, dy: 0)
+    }
+
+    init(dx: Float, dy: Float) {
+        self.dx = dx
+        self.dy = dy
+    }
+}
