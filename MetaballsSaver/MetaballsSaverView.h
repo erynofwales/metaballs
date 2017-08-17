ff853e2c06445806e38e26ecd5b4dd7065f5d5a0
Eryn Wells <eryn@erynwells.me>
2017-08-16 22:03:01 -0700

    [saver] New screensaver

---
+++ MetaballsSaver/MetaballsSaverView.h
@@ -0,0 +1,13 @@
+//
+//  MetaballsSaverView.h
+//  MetaballsSaver
+//
+//  Created by Eryn Wells on 8/16/17.
+//  Copyright © 2017 Eryn Wells. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MetaballsSaverView : ScreenSaverView
+
+@end
---
+++ MetaballsSaver/MetaballsSaverView.m
@@ -0,0 +1,52 @@
+//
+//  MetaballsSaverView.m
+//  MetaballsSaver
+//
+//  Created by Eryn Wells on 8/16/17.
+//  Copyright © 2017 Eryn Wells. All rights reserved.
+//
+
+#import "MetaballsSaverView.h"
+
+@implementation MetaballsSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
