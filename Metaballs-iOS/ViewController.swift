fc0a56711208fd82cb09289d0aaf838bdc9910cb
Eryn Wells <eryn@erynwells.me>
2018-10-07 20:01:08 -0700

    Reorganize existing app files into macOS folder
    - Add an iOS target

---
+++ Metaballs-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Metaballs-iOS
+//
+//  Created by Eryn Wells on 10/7/18.
+//  Copyright © 2018 Eryn Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
