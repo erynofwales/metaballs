0844fbc0106eb0a02335c27bbd9be9dfcbb8aa31
Eryn Wells <eryn@erynwells.me>
2018-10-09 09:28:31 -0700

    Attempting to make iOS work

--- Metaballs-iOS/ViewController.swift
+++ Metaballs-iOS/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private static func defaultParameters() -> Parameters {
+        var p = Parameters()
+        let defaults = UserDefaults.standard
+        let style = defaults.colorStyle ?? .gradient2Horizontal
+        p.colorStyle = style
+        let color0 = defaults.color0 ?? Float4(0.50, 0.79, 1, 1)
+        p.color0 = color0
+        let color1 = defaults.color1 ?? Float4(0.88, 0.50, 1, 1)
+        p.color1 = color1
+        return p
+    }
+
+    internal var field: Field {
+        didSet {
+            field.size = Size(size: metalView.drawableSize)
+        }
+    }
+
+    private var renderer: Renderer!
+
+    internal var metalView: MTKView {
+        return self.view as! MTKView
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        let params = ViewController.defaultParameters()
+        field = Field(parameters: params)
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        let params = ViewController.defaultParameters()
+        field = Field(parameters: params)
+        super.init(coder: coder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        do {
+            renderer = try Renderer(delegate: self)
+        } catch let e {
+            print("\(e)")
+            view = newErrorView()
+            return
+        }
+
+        metalView.delegate = renderer
     }
 
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        renderer.mtkView(metalView, drawableSizeWillChange: metalView.drawableSize)
+        for _ in 1...10 {
+            addBallWithRandomRadius()
+        }
+    }
 
+    // MARK: - Private
+
+    private func newErrorView() -> NSView {
+        let view = NSView()
+        view.layer?.backgroundColor = NSColor.red.cgColor
+        return view
+    }
+
+    private func addBallWithRandomRadius() {
+        let base = UInt32(view.bounds.width * 0.05)
+        let variance = UInt32(base * 2)
+        let r = Float(base + arc4random_uniform(variance))
+        field.add(ballWithRadius: r)
+    }
+
+    // MARK: - RendererDelegate
+
+    var renderSize: Size {
+        get {
+            return field.size
+        }
+        set {
+            field.size = newValue
+        }
+    }
 }
 
